1ed2e0f03e1051e77efa5108754dcaa4033bdbda
coderAung <yewontaung798@gmail.com>
2025-08-01 22:53:04 +0630

    test database migration and seeding with fixtures

--- webapp/travella/controllers/test_controller.py
+++ webapp/travella/controllers/test_controller.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
+from ..domains.models.account_models import Account
+
 
 def home(request):
-    return render(request, 'test/home.html')
+    accounts = Account.objects.all()
+    return render(request, 'test/home.html', {'list': accounts})
 
 def setting(request):
     return render(request, 'test/setting.html')
