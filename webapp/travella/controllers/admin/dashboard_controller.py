53e4980a4621ebba4a3c449703a1c4dfe7873927
coderAung <yewontaung798@gmail.com>
2025-08-05 18:46:30 +0630

    set up django's security

--- webapp/travella/controllers/admin/dashboard_controller.py
+++ webapp/travella/controllers/admin/dashboard_controller.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def dashboard(request:HttpRequest) -> HttpResponse:
     return render(request, 'admin/dashboard.html')
--- webapp/travella/urls.py
+++ webapp/travella/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 
+from travella.routes import auth_urls
+
 from .routes import customer_urls, admin_urls, test_urls
 
 urlpatterns = [
     path('admins/', include(admin_urls)),
     path('customers/', include(customer_urls)),
+    path('auth/', include(auth_urls)),
     path('test/', include(test_urls)),
 ]
