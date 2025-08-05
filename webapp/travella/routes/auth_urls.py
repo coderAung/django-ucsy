b653f892efbd3893c817e5a8f7b593f3227f2c1a
coderAung <yewontaung798@gmail.com>
2025-08-06 00:30:25 +0630

    add test sign out feature

--- webapp/travella/routes/auth_urls.py
+++ webapp/travella/routes/auth_urls.py
@@ -4,5 +4,6 @@ from travella.controllers.auth import auth_controller
 
 
 urlpatterns = [
-    path('sign-in/', auth_controller.sign_in)
+    path('sign-in/', auth_controller.sign_in),
+    path('sign-out/', auth_controller.sign_out, name='sign_out'),
 ]
