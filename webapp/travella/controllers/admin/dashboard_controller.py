c4253863cd80a92932869ec4522d4c0c97508b28
coderAung <yewontaung798@gmail.com>
2025-08-05 22:04:50 +0630

    solve conflict in dashboard_controller

--- webapp/travella/controllers/admin/dashboard_controller.py
+++ webapp/travella/controllers/admin/dashboard_controller.py
@@ -1,10 +1,7 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-<<<<<<< HEAD
 from travella.services.dashboard_service import get_dashboard_data
-=======
 from django.contrib.auth.decorators import login_required
->>>>>>> security
 
 @login_required
 def dashboard(request:HttpRequest) -> HttpResponse:
