58ec74944a6284fed41567abfb4f127d07c9b91c
ShweSin13 <shweella2002@gmail.com>
2025-08-08 02:16:02 +0630

    Date and Time for booking status added

--- webapp/travella/controllers/admin/managements/booking_controller.py
+++ webapp/travella/controllers/admin/managements/booking_controller.py
@@ -1,5 +1,7 @@
 from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render
+from travella.domains.models.booking_models import Booking  
+from django.utils.timezone import localtime
 from travella.dtos.booking_dto import BookingFilterDTO
 from travella.services.booking_service import (
     get_booking_by_id,
@@ -24,20 +26,55 @@ def list(request: HttpRequest) -> HttpResponse:
         bookings_qs = get_all_bookings()
 
     bookings_dtos = get_booking_list_dtos_from_queryset(bookings_qs)
+     # Counts for top display
+    total_count = Booking.objects.count()
+    pending_count = Booking.objects.filter(status=Booking.Status.PENDING).count()
+    reserved_count = Booking.objects.filter(status=Booking.Status.RESERVED).count()
+    cancelled_count = Booking.objects.filter(status=Booking.Status.CANCELLED).count()
+
     return render(request, view('list'), {
         'bookings': bookings_dtos,
         'status': status,
         'query': query,
+        'total_count': total_count,
+        'pending_count': pending_count,
+        'reserved_count': reserved_count,
+        'cancelled_count': cancelled_count,
     })
 
-# GET /admins/bookings/<id>/
 def detail(request: HttpRequest, id: str) -> HttpResponse:
     booking = get_booking_by_id(id)
     if booking is None:
         raise Http404("Booking not found")
 
     total_cost = booking.ticketCount * booking.unitPrice
-    return render(request, view('detail'), {
+
+    context = {
         'booking': booking,
         'total_cost': total_cost,
-    })
+        'pending_date': None,
+        'pending_time': None,
+        'reserved_or_cancelled_date': None,
+        'reserved_or_cancelled_time': None,
+        'reserved_by_name': None,
+    }
+
+    if booking.status == Booking.Status.PENDING:
+        context['pending_date'] = booking.createdAt.date()
+        context['pending_time'] = booking.createdAt.strftime('%I:%M %p').lstrip("0")
+        context['reserved_or_cancelled_date'] = "-"
+        context['reserved_or_cancelled_time'] = "-"
+
+    elif booking.status == Booking.Status.RESERVED:
+        context['reserved_or_cancelled_date'] = booking.statusUpdatedAt.date()
+        context['reserved_or_cancelled_time'] = booking.statusUpdatedAt.strftime('%I:%M %p').lstrip("0")
+        try:
+            context['reserved_by_name'] = booking.history.reservedBy.accountdetail.name
+        except Exception:
+            context['reserved_by_name'] = "-"
+
+    elif booking.status == Booking.Status.CANCELLED:
+        context['reserved_or_cancelled_date'] = booking.statusUpdatedAt.date()
+        context['reserved_or_cancelled_time'] = booking.statusUpdatedAt.strftime('%I:%M %p').lstrip("0")
+
+    return render(request, view('detail'), context)
