e6de550d033c697efa5e87290b6303d163eaffb7
coderAung <yewontaung798@gmail.com>
2025-08-19 00:03:55 +0630

    implement UI of package search filter and scroll animation

--- webapp/travella/controllers/public/public_controller.py
+++ webapp/travella/controllers/public/public_controller.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 from travella.services.package_service import PackageService
+from travella.services.package_utils import load_categories, load_locations
 from travella.utils.route_view import RouteView
 
 view = RouteView.get('public')
@@ -14,7 +15,13 @@ def discover(request:HttpRequest) -> HttpResponse:
 
 def packages(request:HttpRequest) -> HttpResponse:
     _list = packageService.search_for_customer()
-    return render(request, view('packages/list'), {'list': _list})
+    categories = load_categories()
+    locations = load_locations()
+    return render(request, view('packages/list'), {
+        'list': _list,
+        'categories': categories,
+        'locations': locations
+    })
 
 def package_detail(request:HttpRequest, code:str) -> HttpResponse:
     return render(request, view('packages/detail'))
