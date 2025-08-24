06b5f020e42dc8708996a249f0f1eaba1a45bce6
coderAung <yewontaung798@gmail.com>
2025-08-24 11:10:37 +0630

    add next page feature in sign-in

--- webapp/travella/middlewares/auth_middleware.py
+++ webapp/travella/middlewares/auth_middleware.py
@@ -21,7 +21,7 @@ class AuthMiddleWare:
     def __call__(self, request:HttpRequest) -> HttpResponse:
         path = request.path_info
         if not request.user.is_authenticated and not any(path.startswith(url) for url in self.public_urls):
-            return redirect(settings.LOGIN_URL)
+            return redirect(f'{settings.LOGIN_URL}?next={request.path}')
         if request.user.is_authenticated:
             user = request.user
             if user.role == Account.Role.CUSTOMER and path.startswith('/admins/'):
