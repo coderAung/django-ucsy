039afd57ba17b80978be304d6b2f6c574530adf9
HtetNadi3 <htetnadi82@gmail.com>
2025-08-31 15:21:10 +0630

    homepage layout

--- webapp/travella/controllers/public/discover_controller.py
+++ webapp/travella/controllers/public/discover_controller.py
@@ -1,9 +1,11 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from travella.services.package_utils import load_locations
 from travella.utils.route_view import RouteView
 
 view = RouteView.get('public')
 
 def discover(request:HttpRequest) -> HttpResponse:
-    return render(request, view('discover/index'))
+    locations = load_locations()
+    return render(request, view('discover/index'), {'locations':locations})
