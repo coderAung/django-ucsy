9d9092897ae501c2f25190a3d8638feae67ed218
HtetNadi3 <htetnadi82@gmail.com>
2025-09-01 11:59:49 +0630

    most booked packages count 3

--- webapp/travella/controllers/public/discover_controller.py
+++ webapp/travella/controllers/public/discover_controller.py
@@ -3,10 +3,14 @@ from django.shortcuts import render
 
 from travella.services.package_utils import load_categories, load_locations
 from travella.utils.route_view import RouteView
+from travella.services.package_service import PackageService
+
 
 view = RouteView.get('public')
 
 def discover(request:HttpRequest) -> HttpResponse:
     locations = load_locations()
     categories = load_categories()
-    return render(request, view('discover/index'), {'locations':locations, 'categories':categories})
+    most_booked_packages = PackageService().get_most_booked_packages(count=3)
+    print(most_booked_packages)
+    return render(request, view('discover/index'), {'locations':locations, 'categories':categories, 'most_booked_packages': most_booked_packages})
