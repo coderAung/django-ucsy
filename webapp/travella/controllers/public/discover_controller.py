a019857a09db77162c78fbdc1653699e461118db
HtetNadi3 <htetnadi82@gmail.com>
2025-09-01 10:25:17 +0630

    categories in discover

--- webapp/travella/controllers/public/discover_controller.py
+++ webapp/travella/controllers/public/discover_controller.py
@@ -1,11 +1,12 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from travella.services.package_utils import load_locations
+from travella.services.package_utils import load_categories, load_locations
 from travella.utils.route_view import RouteView
 
 view = RouteView.get('public')
 
 def discover(request:HttpRequest) -> HttpResponse:
     locations = load_locations()
-    return render(request, view('discover/index'), {'locations':locations})
+    categories = load_categories()
+    return render(request, view('discover/index'), {'locations':locations, 'categories':categories})
